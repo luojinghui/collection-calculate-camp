952eb24c634b9536e350aa4a7bf5b294e90ec03c
luojinghui <939035243@qq.com>
2015-05-25 15:14:23 +0800

    finsh get_intersection.js

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function get_intersection(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/array');
+    var num_mixed = [];
+
+    num_mixed = _.intersection(collection_b,collection_a);
+    return num_mixed;
 }
 
 module.exports = get_intersection;
