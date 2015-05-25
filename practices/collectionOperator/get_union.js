62bcf6663323c25902424a8d262fdf657cd11278
luojinghui <939035243@qq.com>
2015-05-25 17:32:13 +0800

    finsh get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/array');
+    var array = _.union(collection_a, collection_b);
+
+    return array;
 }
 
 module.exports = get_union;
-
