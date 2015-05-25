24cfe28460845a260648ff2df6b8384cd078d936
luojinghui <939035243@qq.com>
2015-05-25 13:48:46 +0800

    finsh collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function collect_last_element(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/array');
+    var last_number = _.last(collection);
+    
+    return last_number;
 }
 
 module.exports = collect_last_element;
