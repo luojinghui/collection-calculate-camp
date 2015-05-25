2f410223e328e88d56b116dfbc95c158fba3a364
luojinghui <939035243@qq.com>
2015-05-25 14:23:31 +0800

    finsh practices/collectionOperator/get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,7 +2,17 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+    var a = [];
+
+    if(number_a <= number_b) {
+        for(var i=number_a; i<=number_b; i++) {
+            a.push(i);
+        }
+    } else {
+        for(var j=number_a; j>=number_b; j--) {
+        a.push(j);
+        }
+    }
+    return a;
 }
-
 module.exports = get_integer_interval;
-
