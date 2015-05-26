b90428ce45876c50d8c80125c9fe7a3c28b28d7c
luojinghui <939035243@qq.com>
2015-05-26 16:22:46 +0800

    finsh /number_map_to_word.js

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,12 @@
 'use strict';
+function letter(n) {
+    return String.fromCharCode(n+96);
+}
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+    var _ = require('../lodash/');
+    var result = _.map(collection,letter)
+
+    return result;
 };
 
 module.exports = number_map_to_word;
