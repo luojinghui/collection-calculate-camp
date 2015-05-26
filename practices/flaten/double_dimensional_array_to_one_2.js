04062de1e168d96503c4cb6790cc362fdb411bc7
luojinghui <939035243@qq.com>
2015-05-26 15:08:47 +0800

    finsh double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -4,7 +4,8 @@ function double_to_one(collection) {
 
   //在这里写入代码
   var _ = require('../lodash/');
-  
+  var result = _.flatten(collection);
+  return _.union(result);
 }
 
 module.exports = double_to_one;
