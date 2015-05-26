bc63d957c00492c005746c6b459ebe2bdf772621
luojinghui <939035243@qq.com>
2015-05-26 13:35:36 +0800

    finsh grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,7 +2,12 @@
 
 function grouping_count(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/');
+    var result = _.countBy(collection, function(n) {
+        return this.floor(n);
+    },Math);
+    return result;
 }
 
 module.exports = grouping_count;
