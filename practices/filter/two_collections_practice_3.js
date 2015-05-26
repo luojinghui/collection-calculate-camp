ec7db4a704b3e92d0c9c8eeac697083562a49dfb
luojinghui <939035243@qq.com>
2015-05-26 14:45:55 +0800

    finsh two_collections_practice_3.js

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,15 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var _ = require('../lodash/');
+  var result = _.remove(collection_a,function(n) {
+      for(var i=0; i<collection_b.length; i++) {
+          if(n % collection_b[i] === 0) {
+              return true;
+          }
+      }
+  });
+  return result;
 }
 
 module.exports = choose_divisible_integer;
