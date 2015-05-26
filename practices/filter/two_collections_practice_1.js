7abb82ab14798d40b6ae6449ecb3ec31a142a7ee
luojinghui <939035243@qq.com>
2015-05-26 13:36:17 +0800

    finsh two_collections_practice_1.js

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,6 +3,10 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var _ = require('../lodash/');
+  var result = _.intersection(collection_a,collection_b);
+
+  return result;
 }
 
 module.exports = choose_common_elements;
