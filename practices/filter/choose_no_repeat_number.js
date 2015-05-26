4fa8cb4bfa060d1c832745e206e451c24cda7c45
luojinghui <939035243@qq.com>
2015-05-26 12:08:19 +0800

    finsh choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -2,7 +2,11 @@
 
 function choose_no_repeat_number(collection) {
 
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/array');
+    var result = _.uniq(collection);
+
+    return result;
 }
 
 module.exports = choose_no_repeat_number;
