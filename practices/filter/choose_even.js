1896566554cccec19e98f81636592adab1f42497
luojinghui <939035243@qq.com>
2015-05-26 11:51:30 +0800

    finsh choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+    //在这里写入代码
+    var _ = require('../lodash/array');
+    var evens = _.remove(collection, function(n) {
+        return n % 2 == 0;
+    });
+    return evens;
 }
 
 module.exports = choose_even;
