23cef7d6c7a950a4b9a57e21f4b5a4c28b69a833
luojinghui <939035243@qq.com>
2015-05-27 17:43:36 +0800

    add own function for number_map_to_word.js

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -3,9 +3,15 @@ function letter(n) {
     return String.fromCharCode(n+96);
 }
 var number_map_to_word = function(collection){
-    var _ = require('../lodash/');
-    var result = _.map(collection,letter)
+    //var _ = require('../lodash/');
+    //var result = _.map(collection,letter)
 
+    //return result;
+
+    var result = [];
+    for (var i = 0; i < collection.length; i++) {
+        result[result.length] = letter(collection[i]);
+    }
     return result;
 };
 
