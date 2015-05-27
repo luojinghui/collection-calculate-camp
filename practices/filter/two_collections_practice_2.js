fc384be8d1565e32ce7a2b14af3956ce0b19596d
luojinghui <939035243@qq.com>
2015-05-27 13:36:30 +0800

    add own function for two_collections_practice_2.js

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,10 +3,30 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
-  var _ = require('../lodash/');
-  var result = _.xor(collection_a,collection_b);
+//  var _ = require('../lodash/');
+//    var result = _.xor(collection_a,collection_b);
 
-  return result;
+//  return result;
+
+var result = [];
+for(var i=0; i<collection_a.length; i++) {
+    for(var j=0; j<collection_b.length; j++) {
+        if(collection_a[i] === collection_b[j]) {
+            collection_a = remove_element(i,collection_a);
+        }
+    }
+}
+return collection_a;
+}
+
+function remove_element(index,array) {
+    if(index >= 0 && index < array.length) {
+        for(var i=index; i<array.length; i++) {
+            array[i] = array[i+1];
+        }
+            array.length = array.length-1;
+    }
+    return array;
 }
 
 module.exports = choose_no_common_elements;
