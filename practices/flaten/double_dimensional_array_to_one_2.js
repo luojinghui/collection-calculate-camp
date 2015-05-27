bfeeb8d8d8b7a23bfe3d070877f53a5c7a461d98
luojinghui <939035243@qq.com>
2015-05-27 17:07:01 +0800

    add own function for double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,9 +3,28 @@
 function double_to_one(collection) {
 
   //在这里写入代码
-  var _ = require('../lodash/');
-  var result = _.flatten(collection);
-  return _.union(result);
+//  var _ = require('../lodash/');
+//  var result = _.flatten(collection);
+//  return _.union(result);
+
+    var result = [];
+    for (var i = 0; i < collection.length; i++) {
+        for(var j=0; j<collection[i].length; j++) {
+            result[result.length] = collection[i][j];
+        }
+    }
+    return result.unique5();
 }
 
+Array.prototype.unique5 = function() {
+    var res = [], hash = {};
+    for(var i=0, elem; (elem = this[i]) != null; i++)  {
+        if (!hash[elem])
+        {
+            res.push(elem);
+            hash[elem] = true;
+        }
+    }
+    return res;
+}
 module.exports = double_to_one;
