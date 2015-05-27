e4ce7860e5bfd77ca69c894eb1f92c1764d8903b
luojinghui <939035243@qq.com>
2015-05-27 17:13:33 +0800

    add own function for /map_to_four_multiples_add_one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -3,8 +3,14 @@ function twoaddone(n) {
     return n * 4 + 1;
 }
 var map_to_four_multiples_add_one = function(collection){
-    var _ = require('../lodash/');
-    var result = _.map(collection,twoaddone);
+//    var _ = require('../lodash/');
+//    var result = _.map(collection,twoaddone);
+//    return result;
+
+    var result = [];
+    for (var i=0; i<collection.length; i++) {
+        result[result.length]=collection[i] * 4 + 1;
+    }
     return result;
 };
 
