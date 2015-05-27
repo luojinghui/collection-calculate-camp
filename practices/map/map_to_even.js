406d87eb8612f7ae2952e2b62f496f333d7f6b51
luojinghui <939035243@qq.com>
2015-05-27 17:11:19 +0800

    add own function for map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -3,7 +3,13 @@ function two(n) {
     return n * 2;
 }
 function map_to_even(collection){
-    var _ = require('../lodash/');
-    return _.map(collection, two);
+//    var _ = require('../lodash/');
+//    return _.map(collection, two);
+
+    var result = [];
+    for (var i=0; i<collection.length; i++) {
+        result[result.length]=collection[i] * 2;
+    }
+    return result;
 }
 module.exports = map_to_even;
