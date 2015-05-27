95cdb121a65b27073824f0dcf3c8312f145c9c95
luojinghui <939035243@qq.com>
2015-05-27 10:46:36 +0800

    add own function for choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,11 +2,19 @@
 
 function choose_even(collection) {
     //在这里写入代码
-    var _ = require('../lodash/array');
-    var evens = _.remove(collection, function(n) {
-        return n % 2 == 0;
-    });
-    return evens;
+//    var _ = require('../lodash/array');
+//    var evens = _.remove(collection, function(n) {
+//        return n % 2 == 0;
+//    });
+//    return evens;
+
+    var result = [];
+    for(var i=0; i<collection.length; i++) {
+        if(collection[i] % 2 === 0) {
+            result.push(collection[i]);
+        }
+    }
+    return result;
 }
 
 module.exports = choose_even;
