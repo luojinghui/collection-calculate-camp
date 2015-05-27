5447fb64092ed9e56fd89a6e0d491f1b4eeb6781
luojinghui <939035243@qq.com>
2015-05-27 13:22:16 +0800

    add two_collections_practice_1.js

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,10 +3,20 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
-  var _ = require('../lodash/');
-  var result = _.intersection(collection_a,collection_b);
+//  var _ = require('../lodash/');
+//  var result = _.intersection(collection_a,collection_b);
 
-  return result;
+//  return result;
+
+    var result = [];
+    for(var i=0; i<collection_a.length; i++) {
+        for(var j=0; j<collection_b.length; j++) {
+            if(collection_a[i] === collection_b[j]) {
+                result[result.length] = collection_a[i];
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = choose_common_elements;
