672dfac1d1b80874ad38764ecbf45635a6a368c4
luojinghui <939035243@qq.com>
2015-05-27 09:13:47 +0800

    add own function for collect_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,10 +1,18 @@
 'use strict';
 function collect_all_even(collection) {
 //在这里写入代码
-    var _ = require('../lodash/array');
-    var even_number =  _.remove(collection, function(n) {
-        return n % 2 == 0;
-    });
+//    var _ = require('../lodash/array');
+//    var even_number =  _.remove(collection, function(n) {
+//        return n % 2 == 0;
+//    });
+//    return even_number;
+
+    var even_number = [];
+    for(var i=0; i<collection.length; i++) {
+        if(collection[i] % 2 === 0 ) {
+            even_number.push(collection[i]);
+        }
+    }
     return even_number;
 }
 
