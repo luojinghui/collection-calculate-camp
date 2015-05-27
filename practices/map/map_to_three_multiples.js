2e326620002acfe8400781efbffba9cb1e65b7f4
luojinghui <939035243@qq.com>
2015-05-27 17:24:03 +0800

    add own function for map_to_three_multiples.js

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -3,8 +3,13 @@ function four(n) {
     return n * 3;
 }
 var map_to_three_multiples = function(collections){
-    var _ = require('../lodash/');
-    return _.map(collections,four);
+//    var _ = require('../lodash/');
+//    return _.map(collections,four);
+    var result = [];
+    for (var i=0; i<collections.length; i++) {
+        result[result.length] = collections[i] * 3;
+    }
+    return result;
 };
 
 module.exports = map_to_three_multiples;
