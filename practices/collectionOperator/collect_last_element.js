131f96969df4c2fc63f8a439b03a8e4a534eea83
luojinghui <939035243@qq.com>
2015-05-27 09:42:39 +0800

    add own function for collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -2,9 +2,12 @@
 
 function collect_last_element(collection) {
     //在这里写入代码
-    var _ = require('../lodash/array');
-    var last_number = _.last(collection);
-    
+//    var _ = require('../lodash/array');
+//    var last_number = _.last(collection);
+
+//    return last_number;
+
+    var last_number = collection[collection.length-1];
     return last_number;
 }
 
