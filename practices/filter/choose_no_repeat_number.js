f7517f1a41d9a70d0a063306a868cf5def05fd97
luojinghui <939035243@qq.com>
2015-05-27 11:47:27 +0800

    add own function choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,10 +3,26 @@
 function choose_no_repeat_number(collection) {
 
     //在这里写入代码
-    var _ = require('../lodash/array');
-    var result = _.uniq(collection);
+//    var _ = require('../lodash/array');
+//    var result = _.uniq(collection);
 
-    return result;
+//    return result;
+    return collection.remove_same();
+
+
+}
+
+Array.prototype.remove_same = function () {
+    var temp = new Array();
+        this.sort();
+        for(var i = 0; i < this.length; i++) {
+            if( this[i] == this[i+1]) {
+            continue;
+        }
+            temp[temp.length]=this[i];
+      }
+      return temp;
+
 }
 
 module.exports = choose_no_repeat_number;
