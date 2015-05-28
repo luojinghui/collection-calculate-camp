7eb997512c60d57a45e478e0525b0ffafb9ddda1
luojinghui <939035243@qq.com>
2015-05-28 20:25:06 +0800

    alter get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -47,3 +47,30 @@ function get_union(collection_a, collection_b) {
    return all;
     */
 module.exports = get_union;
+
+
+/*
+function grouping_count(collection) {
+  //
+  // //在这里写入代码
+  // var _ = require('../lodash');
+  // return _.countBy(collection);
+  //collection.sort();
+  var temp = {};
+
+  for(var i = 0;i < collection.length;i++) {
+    //   temp[collection[i]]=temp[collection[i]]||0;
+    //   temp[collection[i]]++;
+      if(!! temp[collection[i]]) {
+          temp[collection[i]]++;
+      }else{
+          temp[collection[i]]=0;
+          temp[collection[i]]++;
+      }
+
+  }
+  return temp;
+}
+
+module.exports = grouping_count;
+*/
