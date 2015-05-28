52208b25a2e82954dcc262d0eecf7885148162c1
luojinghui <939035243@qq.com>
2015-05-28 09:47:07 +0800

    finsh collect_max_number.js

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -2,6 +2,13 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var result;
+  for (var i = 0; i < collection.length; i++) {
+      if(collection[i]<collection[i+1]) {
+          result = collection[i+1];
+        }
+     }
+     return result;
 }
 
 module.exports = collect_max_number;
