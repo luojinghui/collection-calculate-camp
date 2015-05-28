05331f33e6365b12e3a7c3689cfa8ecd30df07a3
luojinghui <939035243@qq.com>
2015-05-28 13:38:36 +0800

    alter collect_max_number.js

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,14 +1,17 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
-  var result;
-  for (var i = 0; i < collection.length; i++) {
-      if(collection[i]<collection[i+1]) {
-          result = collection[i+1];
+    var result;
+    if(collection.length === 0) {
+        result = collection[0];
+    }else {
+        for (var i = 0; i < collection.length; i++) {
+            if(collection[i] < collection[i+1]) {
+                result = collection[i+1];
         }
-     }
-     return result;
+    }
+}
+    return result;
 }
 
 module.exports = collect_max_number;
