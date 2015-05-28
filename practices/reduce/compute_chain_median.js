d9ce1b5b63abce0f99df1d9093f0e0b0f84cf4ee
luojinghui <939035243@qq.com>
2015-05-28 20:10:52 +0800

    finsh compute_chain_median.js

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var array_2 = [];
+    var array = [];
+    array_2 =  collection.split('->');
+    var num ;
+    for (var i = 0; i < array_2.length; i++) {
+        array.push(Number(array_2[i]));
+    }
+    sort(array);
+    var ave = (array[array.length / 2 - 1 ] + array[array.length / 2]) / 2;
+    return ave;
+
 }
 
+function sort(array) {
+    for (var i = 0; i < array.length; i ++) {
+        var temp = 0;
+
+        for(var j = i + 1; j < array.length; j ++) {
+            if (array[i] > array[j]) {
+                temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
 module.exports = compute_chain_median;
