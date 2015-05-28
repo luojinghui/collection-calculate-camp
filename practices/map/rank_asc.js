3dac6445694b5ff46539be76d2fb1e443f61b8f1
luojinghui <939035243@qq.com>
2015-05-28 16:29:41 +0800

    alter rank_asc.js

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,9 +1,26 @@
 'use strict';
 var rank_asc = function(collection){
-    return collection.sort(sortNumber);
+//    return collection.sort(sortNumber);
+    var array_sort = [];
+    if(collection.length === 1) {
+        return collection[0];
+    } else {
+    for (var i = 0; i < collection.length; i ++) {
+        var temp = 0;
+
+        for(var j = i + 1; j < collection.length; j ++) {
+            if (collection[i] < collection[j]) {
+                temp = collection[j];
+                collection[j] = collection[i];
+                collection[i] = temp;
+                }
+            }
+        }
+    }
+    return collection;
 };
 
-function sortNumber(c,d) {
-    return d - c ;
-}
+//function sortNumber(c,d) {
+//    return d - c ;
+//}
 module.exports = rank_asc;
