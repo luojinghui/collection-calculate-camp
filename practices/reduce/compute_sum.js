d4342c6a30a2c53037b09b878fd89bd329590389
luojinghui <939035243@qq.com>
2015-05-28 20:14:30 +0800

    finsh practices/reduce/compute_sum.js

--- practices/reduce/compute_sum.js
+++ practices/reduce/compute_sum.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function calculate_elements_sum(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var result = 0;
+    for (var i = 0; i < collection.length; i++) {
+        result += collection[i];
+    }
+    return result;
 }
 
 module.exports = calculate_elements_sum;
-
