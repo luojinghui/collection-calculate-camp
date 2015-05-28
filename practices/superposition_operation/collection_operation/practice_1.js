23799035205417f83a38b1feaa159df54ee90e54
luojinghui <939035243@qq.com>
2015-05-28 21:10:46 +0800

    finsh practice_1.js

--- practices/superposition_operation/collection_operation/practice_1.js
+++ practices/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,12 @@
 'use strict';
 
 function hybrid_operation(collection) {
+    var sum = 0;
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        sum += (collection[i] * 3 + 2);
+    }
+    return sum;
 }
 
 module.exports = hybrid_operation;
-
