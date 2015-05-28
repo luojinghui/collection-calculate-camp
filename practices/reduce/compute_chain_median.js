9383c143b9da36e66e0e2f3c28ba69aa6f177148
luojinghui <939035243@qq.com>
2015-05-28 20:26:15 +0800

    alter compute_chain_median.js

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -29,3 +29,45 @@ function sort(array) {
     }
 }
 module.exports = compute_chain_median;
+
+/*
+'use strict';
+
+function compute_chain_median(collection) {
+    var array = [];
+    var num = "";
+
+    for(var i = 0; i < collection.length; i++) {
+        if(parseInt(collection[i]) === parseInt(collection[i])) {
+            num += collection[i];
+        }else {
+            if(num.length != 0){
+                array[array.length] = parseInt(num);
+                num = "";
+            }
+        }
+        if(i === collection.length-1){
+            array[array.length] = parseInt(num);
+        }
+    }
+
+    for(var i = 0; i < array.length; i++) {
+        for(var j = i; j < array.length; j++) {
+            if(array[i] > array[j]) {
+                array[i] = array[i] + array[j];
+                array[j] = array[i] - array[j];
+                array[i] = array[i] - array[j];
+            }
+        }
+    }
+
+    if(array.length % 2 === 0) {
+        return (array[array.length/2-1]+array[array.length/2])/2;
+    }else{
+        return array[parseInt(array.length/2)];
+    }
+
+}
+
+module.exports = compute_chain_median;
+*/
