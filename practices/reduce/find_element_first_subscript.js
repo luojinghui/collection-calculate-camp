5609991dd4245c221248e63d12aecc7104e6c8f0
luojinghui <939035243@qq.com>
2015-05-28 20:18:34 +0800

    finsh ind_element_first_subscript.js

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-  //在这里写入代码
+    //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if(element === collection[i]) {
+            return i;
+        }
+    }
 }
 
 module.exports = calculate_elements_sum;
-
