d5c814d88132667eb2aaab0579f14baa82c99ffd
luojinghui <939035243@qq.com>
2015-05-28 09:38:19 +0800

    add own function for grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,10 +3,18 @@
 function grouping_count(collection) {
 
     //在这里写入代码
-    var _ = require('../lodash/');
-    var result = _.countBy(collection, function(n) {
-        return this.floor(n);
-    },Math);
+//    var _ = require('../lodash/');
+//    var result = _.countBy(collection, function(n) {
+//        return this.floor(n);
+//    },Math);
+//    return result;
+
+    var result = {};
+    for (var i = 0; i < collection.length; i++) {
+        result[collection[i]] = result[collection[i]] || 0;
+        result[collection[i]] ++;
+
+    }
     return result;
 }
 
