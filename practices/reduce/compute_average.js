6ab090a9a221381d214e38e59210e5b11955ff61
luojinghui <939035243@qq.com>
2015-05-28 15:36:07 +0800

    finsh compute_average.js

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+    //在这里写入代码
+    var num = 0;
+    var result = 0;
+    for (var i = 0; i < collection.length; i++) {
+        num += collection[i];
+    }
+    result = num / collection.length;
+    return result;
 }
 
 module.exports = compute_average;
-
