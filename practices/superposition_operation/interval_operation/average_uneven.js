6f258c62d57bcfd9a686f77998ec913b36d7d0b9
luojinghui <939035243@qq.com>
2015-05-29 09:18:23 +0800

    finsh average_uneven.js

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function average_uneven(collection) {
+    var ave = 0;
+    var sum = 0;
+    var count = 0
 
-  //在这里写入代码
+    for (var i = 0; i < collection.length; i++) {
+        if (collection[i] % 2 != 0) {
+            sum += collection[i];
+            count ++;
+        }
+    }
+    ave = sum / count;
+    return ave;
 }
 
 module.exports = average_uneven;
