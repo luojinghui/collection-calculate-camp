4526cb617b1552a9714d2f534f6d08af586027d3
luojinghui <939035243@qq.com>
2015-05-30 20:31:47 +0800

    finsh one_add_next_multiply_three.js

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,14 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+    var array = [];
+    var temp = 0;
+
+    for (var i = 0; i < collection.length; i++) {
+        if(i < collection.length - 1) {
+            temp = (collection[i] + collection[i+1]) * 3;
+            array.push(temp);
+        }
+    }
+    return array;
 }
 module.exports = one_add_next_multiply_three;
