eb17d10904f8b55dfbfc782480791a85618f2420
luojinghui <939035243@qq.com>
2015-05-30 17:41:19 +0800

    finsh is_exist_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,19 @@
 'use strict';
 var is_exist_element = function(collection,element){
+    for (var i = 0; i < collection.length; i++) {
+        if (i % 2 === 0) {
+            if (collection[i] === element) {
+                return true;
+            }
+        }
+    }
 
+    for (var j = 0; j < collection.length; j++) {
+        if (j % 2 === 0) {
+            if (collection[j] != element) {
+                return false;
+            }
+        }
+    }
 };
 module.exports = is_exist_element;
