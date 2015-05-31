b59140a7bfb5c5bb8a65406fc155a26f6c7ffebc
luojinghui <939035243@qq.com>
2015-05-31 12:01:01 +0800

    alter map_to_four_multiples_add_one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,18 +1,8 @@
 'use strict';
-function twoaddone(n) {
-    return n * 4 + 1;
-}
 var map_to_four_multiples_add_one = function(collection){
-//    var _ = require('../lodash/');
-//    var result = _.map(collection,twoaddone);
-//    return result;
-
-    var result = [];
-    for (var i=0; i<collection.length; i++) {
-        result[result.length]=collection[i] * 4 + 1;
-    }
-    return result;
-};
-
-
+    var map = require('../lodash/map.js');
+    return map(collection,function(n) {
+        return n * 4 + 1;
+    });
+}
 module.exports = map_to_four_multiples_add_one;
