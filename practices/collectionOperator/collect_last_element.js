7c53aa87abfbb7990a54a4e37a3b638e7140c27b
luojinghui <939035243@qq.com>
2015-06-01 17:33:13 +0800

    mylodash for collect_last_element.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -7,8 +7,13 @@ function collect_last_element(collection) {
 
 //    return last_number;
 
-    var last_number = collection[collection.length-1];
-    return last_number;
+//    var last_number = collection[collection.length-1];
+//    return last_number;
+
+    var _ = require('../lodash/mylodash.js');
+    return _.reduce(collection,function(a,b) {
+        return b;
+    })
 }
 
 module.exports = collect_last_element;
