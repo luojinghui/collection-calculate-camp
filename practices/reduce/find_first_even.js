aeb04e3f1d50d27718bc30ee7390f2cec812ec6b
luojinghui <939035243@qq.com>
2015-06-01 15:52:40 +0800

    add mylodash for find_first_even.js

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,11 +2,21 @@
 
 function find_first_even(collection) {
     //在这里写入代码
-    for (var i = 0; i < collection.length; i++) {
-        if (collection[i] % 2 === 0) {
-            return collection[i];
+    // for (var i = 0; i < collection.length; i++) {
+    //     if (collection[i] % 2 === 0) {
+    //         return collection[i];
+    //     }
+    // }
+    var _ = require('../lodash/mylodash.js');
+    var array = _.filter(collection,function(n) {
+        if(n % 2 === 0 ) {
+            return n;
         }
-    }
+    })
+
+    return _.reduce(array,function(a,b) {
+        return a;
+    });
 }
 
 module.exports = find_first_even;
