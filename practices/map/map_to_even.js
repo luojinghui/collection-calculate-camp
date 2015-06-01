4561b46b6f5eb11e106a162fe8e2f67b7caca910
luojinghui <939035243@qq.com>
2015-06-01 14:02:50 +0800

    _.map(c,f) for map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,9 +1,9 @@
 'use strict';
 function map_to_even(collection){
-    var map = require('../lodash/map.js');
-    return map(collection,function(n) {
-        return n * 2;
-    });
+    var _ = require('../lodash/mylodash.js');
 
+    return _.map(collection,function(n) {
+        return n * 2;
+});
 }
 module.exports = map_to_even;
