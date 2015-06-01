7b3bdff2fa0d9d5b9eb4d710119eeace2847f8eb
luojinghui <939035243@qq.com>
2015-06-01 16:18:43 +0800

    add mylodash for collect_min_number.js

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -2,13 +2,19 @@
 
 function collect_min_number(collection) {
     //在这里写入代码
-    var result = collection[0];
-    for(var i=0; i<collection.length; i++) {
-        if (collection[i] < result) {
-            result = collection[i];
-        }
-    }
-    return result;
+    // var result = collection[0];
+    // for(var i=0; i<collection.length; i++) {
+    //     if (collection[i] < result) {
+    //         result = collection[i];
+    //     }
+    // }
+    // return result;
+
+    var _ = require('../lodash/mylodash.js');
+
+    return _.reduce(collection,function(a,b) {
+        return Math.min(a,b);
+    })
 }
 
 module.exports = collect_min_number;
