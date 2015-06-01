ed5bae30d26a31dfa9c3a7068bdd6474d644183b
luojinghui <939035243@qq.com>
2015-06-01 17:28:51 +0800

    alter mylodash add note

--- practices/lodash/mylodash.js
+++ practices/lodash/mylodash.js
@@ -1,15 +1,14 @@
-//  通过fun函数映射某一集合；
 
-function _() {
-
-}
+function _() {}
 
 _.foreach = function(collection,fun) {
    for (var i = 0; i < collection.length; i++) {
        fun(collection[i],i);
    }
 }
-
+/*
+map方法是映射数组，不会改变数组的长度，只会改变数组中对应元素。
+*/
  _.map = function(array,fun) {
     var result = [];
 
@@ -20,6 +19,9 @@ _.foreach = function(collection,fun) {
     return result;
 }
 
+/*
+filter方法是可以改变数组，通过某种function方法返回true和false，若是true，则将数组push进去，并返回。
+*/
 _.filter = function(collection,fun) {
     var result = [];
 
@@ -31,6 +33,9 @@ _.filter = function(collection,fun) {
     return result;
 }
 
+/*
+reduce方法是通过一个function的到数组中的某一个元素。
+*/
  _.reduce = function(array,fun) {
     var result = array[0];
 
@@ -44,28 +49,4 @@ _.filter = function(collection,fun) {
     return result;
 }
 
-/*
-_.add = function(a,b) {
-   return a + b;
-}
-
-_.max = function(a,b) {
-   if(a > b) {
-       return a;
-   }
-}
-
-_.max = function(a,b) {
-   if(a < b) {
-       return a;
-   }
-}
-
-_.last = function(a,b) {
-   if(b % 2 === 0) {
-       a = b;
-       return a;
-   }
-}
-*/
 module.exports = _;
