05c51355c77c0298ee5fa306a9443c2c1b26b40d
luojinghui <939035243@qq.com>
2015-06-03 16:22:30 +0800

    alter letter_mapping_2 due to mylodash

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -2,11 +2,13 @@
 
 function even_to_letter(collection) {
     var array = [];
-    for (var i = 0; i < collection.length; i++) {
-        if(collection[i] % 2 === 0 ) {
+    var _ = require('../../lodash/mylodash.js');
+
+    _.foreach(collection,function(n,i) {
+        if(n % 2 === 0 ) {
             array[array.length] = String.fromCharCode(i + 97);
         }
-    }
+    });
     return array;
 }
 
