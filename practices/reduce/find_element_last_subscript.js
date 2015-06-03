24061eb6f8dec85dc169d539c20fdf40c265b66a
luojinghui <939035243@qq.com>
2015-06-03 16:32:01 +0800

    alter find_element_last_subscript.js

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -2,11 +2,21 @@
 
 function calculate_elements_sum(collection, element) {
     //在这里写入代码
-    for (var i = collection.length - 1; i >= 0; i--) {
-        if(element === collection[i]) {
-            return i;
+    var index = 0;
+    var _ = require('../lodash/mylodash.js');
+
+    _.foreach(collection,function(n,i) {
+        if(n === element) {
+            index = i;
         }
-    }
+    });
+    return index;
+
+    // for (var i = collection.length - 1; i >= 0; i--) {
+    //     if(element === collection[i]) {
+    //         return i;
+    //     }
+    // }
 }
 
 module.exports = calculate_elements_sum;
