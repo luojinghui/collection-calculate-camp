4a3d6360122fef62b79174601553554dd2a91f41
luojinghui <939035243@qq.com>
2015-06-03 14:36:00 +0800

    alter for compute_average.js

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,13 +2,21 @@
 
 function compute_average(collection) {
     //在这里写入代码
-    var num = 0;
-    var result = 0;
-    for (var i = 0; i < collection.length; i++) {
-        num += collection[i];
-    }
-    result = num / collection.length;
-    return result;
+    // var num = 0;
+    // var result = 0;
+    // for (var i = 0; i < collection.length; i++) {
+    //     num += collection[i];
+    // }
+    // result = num / collection.length;
+    // return result;
+
+    var _ =require('../lodash/mylodash.js');
+    var sum;
+
+    sum = _.reduce(collection,function(a,b) {
+        return a + b;
+    });
+    return (sum / collection.length);
 }
 
 module.exports = compute_average;
