3b1db1e44daf7baafa15aa52ef2c7f4b5e09ce76
luojinghui <939035243@qq.com>
2015-06-03 16:23:01 +0800

    n

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -2,11 +2,14 @@
 
 function average_to_letter(collection) {
     var sum = 0 ;
+    var _ = require('../../lodash/mylodash.js');
 
-    for (var i = 0; i < collection.length; i++) {
-        sum += collection[i];
-    }
+    _.foreach(collection,function(n) {
+        sum += n;
+    });
+
     var ave = Math.ceil( sum / (collection.length ) );
+
     return String.fromCharCode(ave + 96);
 }
 
